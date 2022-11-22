c976a49a8f1b7433d17635a2c5a2a476f72dfb60
Lea <lea.delaporte96@hotmail.fr>
2022-11-22 12:13:52 +0100

    first

---
+++ backend/app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const path = require('path');
+
+const sauceRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');
+
+const app = express();
+
+app.use(express.json());
+
+// Protection contre certaines vulnérabilité connues
+app.use(helmet());
+
+
+mongoose.connect('mongodb+srv://lea1:lea1@cluster0.rx0z68w.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() =>console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée!'));
+
+
+// Pour empêcher les erreurs de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    next();
+});
+
+// Protection contre l'injection en remplaçant les caractères interdits
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
+
+// Définition des accès principaux
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
+
+
+
+module.exports = app;
